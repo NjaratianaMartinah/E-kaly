093a08f5d2dafbe2905d13af0285e6e172816a55
Njaratiana <stagiaire.it@ravinala-airports.aero>
2022-04-04 19:50:16 +0200

    config base et user api

--- api/modules/app/app.module.js
+++ api/modules/app/app.module.js
@@ -3,9 +3,9 @@ const cors = require('cors');
 const app = express();
 
 app.use('/public', express.static('public'));
-app.use(express.static('dist/ekaly-fo'));
+app.use(express.static('dist/ekaly-front'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(express.urlencoded( { extended : false }));
+app.use( cors() );
 
 module.exports = { app };
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const { main, app } = require('./api/main');
-const path = require('path');
-app.get('/ekaly/*', (req, res) => res.sendFile(path.join(__dirname, 'dist/ekaly-fo/index.html')));
-app.get('/ekaly/', (req, res) => res.sendFile(path.join(__dirname, 'dist/ekaly-fo/index.html')));
+const {main , app} = require('./api/main');
+const path= require('path');
+
+
+app.get('/ekaly/*', (req, res) => {
+  console.log("Ok ok");
+  res.sendFile(path.join(__dirname, 'dist/ekaly-front/index.html'))
+});
+
+app.get('/ekaly/', (req, res) =>{
+  console.log("Ok ok");
+  res.sendFile(path.join(__dirname, 'dist/ekaly-front/index.html'))
+});
+
 main();
+
